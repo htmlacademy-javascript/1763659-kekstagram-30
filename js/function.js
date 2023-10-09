dcdf33e6b569886df98c49f1f7ac50d305ef8f3b
almazzar <almazzar@gmail.com>
2023-10-09 22:39:54 +0200

    Первая задача готова

--- js/function.js
+++ js/function.js
@@ -1,19 +1,15 @@
-function getStringLength (string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return false;
+// Проверка длинны строки
+function getStringLength(string, maxLength) {
+  return string.length <= maxLength;
 }
+  console.log(getStringLength('asdadasda3', 20));
 
-string('asdadasda3');
-maxLength(20);
+// Проверка на полиндром
+const stringSource = 'Лёша на полке клопа нашёл';
+let stringClean = stringSource.toLowerCase().replaceAll(' ', '');
+  console.log(stringClean);
 
-console.log('Длина строки ' + getStringLength + ' символов');
-
-function polindromString (string) {
-return(string.replaceAll());
-return(string.toUpperCase());
-}
-let string = clearString;
-
-string('Лёша на полке клопа нашёл ');
+let charAmount = stringClean.length - 1;
+  for (let i = charAmount; i > 0; i--) {
+    console.log(stringClean[i]);
+let
