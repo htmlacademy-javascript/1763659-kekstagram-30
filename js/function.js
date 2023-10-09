36dc875631d3700f7206b4ea5d8b878d04728ddd
almazzar <almazzar@gmail.com>
2023-10-09 15:21:41 +0200

    Добавил funstion.js в index.html

---
+++ js/function.js
@@ -0,0 +1,19 @@
+function getStringLength (string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+string('asdadasda3');
+maxLength(20);
+
+console.log('Длина строки ' + getStringLength + ' символов');
+
+function polindromString (string) {
+return(string.replaceAll());
+return(string.toUpperCase());
+}
+let string = clearString;
+
+string('Лёша на полке клопа нашёл ');
