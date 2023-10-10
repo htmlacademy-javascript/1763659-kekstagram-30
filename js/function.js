bb246eaf938077887ea17b4bbdf47d60c1658bba
almazzar <almazzar@gmail.com>
2023-10-10 14:37:14 +0200

    Дополнительное задание готово

--- js/function.js
+++ js/function.js
@@ -4,6 +4,7 @@ function getStringLength(string, maxLength) {
 }
   console.log(getStringLength('asdadasda3', 20));
 
+
 // Проверка на полиндром
 const strSource = 'Лёша на полке клопа нашёл';
 const strClean = strSource.toLowerCase().replaceAll(' ', '');
@@ -18,3 +19,29 @@ function getStringCompare(strClean, strReverse) {
   return strClean === strReverse();
 }
 console.log(getStringCompare(strClean, strReverse));
+
+
+// Возврат целого положительного числа
+function getNumber(strRandom) {
+  let result = '';
+  for (let i = 0; i <= strRandom.length - 1; i++) {
+    const strNumber = parseInt(strRandom[i], 10);
+    if (!isNaN(strNumber)) {
+      result += strNumber;
+    }
+  }
+  if(result === '') {
+    return NaN;
+  }
+  return result;
+}
+console.log(getNumber('2023 год'));
+console.log(getNumber('ECMAScript 2022'));
+console.log(getNumber('1 кефир, 0.5 батона'));
+console.log(getNumber('агент 007'));
+console.log(getNumber('а я томат'));
+console.log(getNumber(-1));
+
+// Функция извлечения цифр от 0 до 9
+// Возврат в виде положительного числа
+// Если в строке нет цифр, функция должна вернуть NaN;
